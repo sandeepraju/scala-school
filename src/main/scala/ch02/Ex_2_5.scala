b4a39ec1b4807cde2c2cf797fe5217eac4f06109
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-17 00:01:31 -0800

    more tests

--- src/main/scala/ch02/Ex_2_5.scala
+++ src/main/scala/ch02/Ex_2_5.scala
@@ -2,11 +2,12 @@ package ch02
 
 object Ex_2_5 {
   def main(args: Array[String]): Unit = {
-    val twox = (s: String) => s * 2
-    val threex = (s: String) => s * 3
+    val twice = (s: String) => s * 2
+    val thrice = (s: String) => s * 3
 
-    assert(compose(twox, threex)("") == "")
-    assert(compose(twox, threex)("*") == "******")
+    assert(compose(twice, thrice)("") == "")
+    assert(compose(twice, thrice)("*") == "******")
+    assert(compose(thrice, twice)("*") == "******")
   }
 
   def compose[A, B, C](f: B => C, g: A => B): A => C = {
