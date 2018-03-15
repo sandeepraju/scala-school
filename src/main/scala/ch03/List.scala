a07ae97239c03c11ccbd10b8c2f83eb3e34bcfbc
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-15 15:21:32 -0700

    Ex 3.8, 3.9 and 3.10

--- src/main/scala/ch03/List.scala
+++ src/main/scala/ch03/List.scala
@@ -1,5 +1,7 @@
 package ch03
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -12,7 +14,7 @@ object List {
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
-    case Cons(0.0, _) => 0.0
+    case Cons(0.0, _) => 0.0 // short circuit
     case Cons(x, xs) => x * product(xs)
   }
 
@@ -56,6 +58,30 @@ object List {
     case Cons(h, t) => Cons(h, init(t))
   }
 
+  // Listing 3.2
+  // Cons(1, Cons(2, Nil)) <===> f(1, f(2, z))
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
   // Ex 3.7
-  
+  // Doesn't look possible in the current implementation
+
+  // Ex 3.8
+  // Returns the same list we passed.
+  // Shows 1:1 mapping between data constructors of List
+  // and foldRight (they are essentially the same)
+
+  // Ex. 3.9
+  def length[A](as: List[A]): Int = foldRight(as, 0) { (_, s) => s + 1 }
+
+  // Ex. 3.10
+  // Easy approach is to reverse and perform foldRight
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 }
