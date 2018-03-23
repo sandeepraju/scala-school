66554dec28f44865079d819f1800ec37c52607a8
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-22 23:55:32 -0700

    Ex 3.9 test cases

--- src/main/scala/ch03/Ex_3_9.scala
+++ src/main/scala/ch03/Ex_3_9.scala
@@ -2,6 +2,9 @@ package ch03
 
 object Ex_3_9 {
   def main(args: Array[String]): Unit = {
-
+    assert(List.length(List()) == 0)
+    assert(List.length(List(1)) == 1)
+    assert(List.length(List(1, 2)) == 2)
+    assert(List.length(List(1, 2, 3, 4)) == 4)
   }
 }
