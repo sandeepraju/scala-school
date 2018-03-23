ad118d07699a319d73c50bd12418efb40fb228e2
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-23 00:11:02 -0700

    Ex 3.10 tests

--- src/main/scala/ch03/Ex_3_10.scala
+++ src/main/scala/ch03/Ex_3_10.scala
@@ -2,6 +2,10 @@ package ch03
 
 object Ex_3_10 {
   def main(args: Array[String]): Unit = {
-
+    def f(b: String, a: Int) =  b + a.toString
+    assert(List.foldLeft(List(), "")(f) == "")
+    assert(List.foldLeft(List(1), "")(f) == "1")
+    assert(List.foldLeft(List(1, 2), "")(f) == "12")
+    assert(List.foldLeft(List(4, 5, 3), "")(f) == "453")
   }
 }
