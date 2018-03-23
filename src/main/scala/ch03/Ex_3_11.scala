f4aac2308a98f0cb742efd6696424e430c8b172c
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-23 00:15:52 -0700

    Ex 3.11 tests

--- src/main/scala/ch03/Ex_3_11.scala
+++ src/main/scala/ch03/Ex_3_11.scala
@@ -2,6 +2,9 @@ package ch03
 
 object Ex_3_11 {
   def main(args: Array[String]): Unit = {
-
+    assert(List.sum2(List()) == 0)
+    assert(List.sum2(List(1)) == 1)
+    assert(List.sum2(List(3, 3)) == 6)
+    assert(List.sum2(List(1, 2, 3)) == 6)
   }
 }
