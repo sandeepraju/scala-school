82fb3880387fb4640c0891f5a274c09abbcfea49
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-22 23:44:02 -0700

    Ex 3.4 tests

--- src/main/scala/ch03/Ex_3_4.scala
+++ src/main/scala/ch03/Ex_3_4.scala
@@ -1,7 +1,23 @@
 package ch03
 
+import org.scalatest.Assertions.assertThrows
+
 object Ex_3_4 {
   def main(args: Array[String]): Unit = {
-
+    assert(List.drop(List(), 0) == List())
+    assertThrows[IllegalArgumentException] {
+      List.drop(List(), 5)
+    }
+    assertThrows[IllegalArgumentException] {
+      List.drop(List(1), 5)
+    }
+    assert(List.drop(List(1), 0) == List(1))
+    assert(List.drop(List(1), 1) == List())
+    assert(List.drop(List(1, 2, 3), 0) == List(1, 2, 3))
+    assert(List.drop(List(1, 2, 3), 1) == List(2, 3))
+    assert(List.drop(List(1, 2, 3), 3) == List())
+    assertThrows[IllegalArgumentException] {
+      List.drop(List(1, 2, 3), 4)
+    }
   }
 }
