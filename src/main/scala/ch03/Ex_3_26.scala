1a8cc1ce37d6d3f8a91c771c1d82cb2bb9687bbc
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-03-29 17:15:16 -0700

    tree problem stubs

---
+++ src/main/scala/ch03/Ex_3_26.scala
@@ -0,0 +1,7 @@
+package ch03
+
+object Ex_3_26 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
--- src/main/scala/ch03/Tree.scala
+++ src/main/scala/ch03/Tree.scala
@@ -5,8 +5,21 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
+  // Ex. 3.25
   def size[A](tree: Tree[A]): Int = tree match {
     case Leaf(_) => 1
     case Branch(l, r) => size(l) + size(r) + 1
   }
+
+  // Ex. 3.26
+  def maximum(tree: Tree[Int]): Int = ???
+
+  // Ex. 3.27
+  def depth[A](tree: Tree[A]): Int = ???
+
+  // Ex. 3.28
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = ???
+
+  // Ex. 3.29
+  def fold[A, B](tree: Tree[A], acc: B)(f: (A, B) => B): B = ???
 }
